c8330a88782927f406de02462a1b7a6497f708e0
Kirk Daniel Caponpon <caponponkirk24@gmail.com>
2021-12-28 14:50:04 -0500

    Attempting to add a currency system into the game

---
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/Kirk C. Folder/Currency.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Currency : MonoBehaviour
+{
+    //public static float gold;
+    //GameObject currencyUI;
+
+    //void Start()
+    //{
+    //    currencyUI = GameObject.Find("Currency"); 
+
+    //}
+    //void Update() {
+    //    currencyUI.GetComponent<Text>().text = gold.ToString(); 
+
+    //    if (gold < 0)
+    //    {
+    //        gold = 0;
+    //    }
+    //} 
+
+}
---
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/Kirk C. Folder/PickUpMoney.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpMoney : MonoBehaviour
+{
+    //Currency script;
+
+    //public static float addAmount;
+
+    //void Start()
+    //{
+    //    script = GameObject.FindWithTag("GameController").GetComponent<Currency>();
+    //}
+
+    //void OnTriggerEnter2D(Collider2D obj)
+    //{
+    //    if (obj.gameObject.tag == "Player")
+    //    {
+    //        script.gold += addAmount;
+    //        Destroy(gameObject);
+    //    }
+    //}
+}
