54bb4d59b189f101afd76389f586aba0e2107d07
ajatucker <ajatucker@outlook.com>
2022-01-02 20:28:30 -0500

    Added Alexis folder

---
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/Enemy and LevelTeamScenes/Devin, Level Scene/Alexis Folder/DropCurrency.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropCurrency : MonoBehaviour
+{
+    public int energy = 100;
+
+    public int currentEnergy = 0;
+
+    public int credits = 1;
+
+    private bool dropOnce = false;
+
+
+    [Header("Credit")]
+
+    [SerializeField] protected Credit creditPrefab;
+
+    [SerializeField] protected int creditVal;
+
+    void Start()
+    {
+        currentEnergy = energy;
+        creditVal = 1;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            DamageRobot(10);
+        }
+    }
+
+
+
+    public void DamageRobot(int damage)
+    {
+        currentEnergy -= damage;
+
+        if (currentEnergy <= 0 && dropOnce == false)
+        {
+            InstantiateCredit();
+
+            Destroy(gameObject);
+
+            dropOnce = true;
+        }
+    }
+
+
+
+    protected virtual void InstantiateCredit()
+    {
+        var credit = Instantiate(creditPrefab.gameObject, transform.position, Quaternion.identity);
+
+        credit.GetComponent<Credit>().SetCreditValue(creditVal);
+    }
+
+}
