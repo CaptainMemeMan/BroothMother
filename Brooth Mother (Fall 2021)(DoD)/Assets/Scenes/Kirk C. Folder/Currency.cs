4d8c18860b90ba25a673615571f45dd0050a3260
NamelessGod95 <72949814+NamelessGod95@users.noreply.github.com>
2022-01-05 17:40:05 -0500

    fixed currency

--- Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/Kirk C. Folder/Currency.cs
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/Kirk C. Folder/Currency.cs
@@ -23,4 +23,12 @@ public class Currency : MonoBehaviour
         }
     }
 
+    public float returngold() {
+
+
+        return gold;
+    
+    }
+
+
 }
--- Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/TowerTeamScenes/Par's Scene/Shop.cs
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/TowerTeamScenes/Par's Scene/Shop.cs
@@ -6,53 +6,51 @@ using UnityEngine;
 public class Shop : MonoBehaviour
 {
 
+    Currency money;
     public GameObject sr_tower;
     public GameObject lr_tower;
     public GameObject bomb_tower;
     public GameObject farm_tower;
-    
+   
 
-    public void Start()
-    {
-       
-    }
 
     // Start is called before the first frame update
     public void buy_LR()
     {
-        if (Curreny.gold  >= 70)
+       
+        if ( money.returngold() >= 70)
         {
+            Currency.gold -= 70;
             GetComponentInParent<PlayerMovement>().towercarry(lr_tower);
         }
     }
 
     public void buy_SR()
     {
-        if (Curreny.gold >= 50)
+        if (money.returngold() >= 50)
         {
+            Currency.gold -= 50;
             GetComponentInParent<PlayerMovement>().towercarry(sr_tower);
         }
     }
     public void buy_bomb()
     {
-        if (Curreny.gold >= 30)
+        if (money.returngold() >= 30)
         {
+            Currency.gold -= 30;
             GetComponentInParent<PlayerMovement>().towercarry(bomb_tower);
         }
     }
 
     public void buy_PP()
     {
-        if (Curreny.gold >= 100)
+        if (money.returngold() >= 100)
         {
+            Currency.gold -= 100;
             GetComponentInParent<PlayerMovement>().towercarry(farm_tower);
         }
     }
 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
