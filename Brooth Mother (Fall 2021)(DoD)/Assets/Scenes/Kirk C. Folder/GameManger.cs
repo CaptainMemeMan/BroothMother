1b79fac94181ba0d1088da25867702c7f481e5d0
Kirk Daniel Caponpon <caponponkirk24@gmail.com>
2022-01-06 19:37:58 -0500

    gamemanger

---
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/Kirk C. Folder/GameManger.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class GameManger : MonoBehaviour
+{
+    //bool gameHasEnded = false; 
+    //public void EndGame()
+    //{
+    //    if(gameHasEnded == false)
+    //    {
+    //        gameHasEnded = true;
+    //        Debug.Log("fornite");
+
+    //    }
+       
+    //}
+
+    //private void Reset()
+    //{
+        
+    //}
+}
