f70e15769a014bc99ce5928b3499561042a2b19a
NamelessGod95 <jaxonpecora@gmail.com>
2022-01-06 22:04:49 -0500

    added credit

--- Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/TowerTeamScenes/Michelle S. Folder/SR Tower/Bullet.cs
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/TowerTeamScenes/Michelle S. Folder/SR Tower/Bullet.cs
@@ -33,6 +33,7 @@ public class Bullet : MonoBehaviour
             collision.GetComponent<EnemyPath>().updatehealth(damage);
         
         }
+
         //else if (collision.gameObject.tag == "Player")
         //{
         //    collision.GetComponent<PlayerHealth>().DamagePlayer(1);
