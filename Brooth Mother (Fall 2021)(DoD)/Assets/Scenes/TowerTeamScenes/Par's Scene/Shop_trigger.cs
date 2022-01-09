34ab6501e725962b41503d53c1a686346f8024cf
Kirk Daniel Caponpon <caponponkirk24@gmail.com>
2022-01-08 20:12:25 -0500

    shop trigger fixed

--- Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/TowerTeamScenes/Par's Scene/Shop_trigger.cs
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/TowerTeamScenes/Par's Scene/Shop_trigger.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Shop_trigger : MonoBehaviour
 {
     public Shop ShopCustomer;
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
