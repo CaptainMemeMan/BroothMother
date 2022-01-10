4e24e0e5d2364232e8673dac272659673be37efb
NamelessGod95 <jaxonpecora@gmail.com>
2022-01-09 19:41:36 -0500

    rotation to tower placement

--- Brooth Mother (Fall 2021)(DoD)/Assets/rotate.cs
+++ Brooth Mother (Fall 2021)(DoD)/Assets/rotate.cs
@@ -21,17 +21,20 @@ public class rotate : MonoBehaviour
         Vector3 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
+        if (Input.GetKeyDown(KeyCode.R)) {
+            changpos(90);
+        
+        }
 
        
        
 
     }
-    //public void changpos(float anger)
-    //{
-    //    transform.RotateAround(target.transform.position, new Vector3(0, 0, 1), anger);
+    public void changpos(float anger)
+    {
+        transform.RotateAround(target.transform.position, new Vector3(0, 0, 1), anger);
 
-    //}
+    }
 
     void OnTriggerStay2D(Collider2D collision)
     {
