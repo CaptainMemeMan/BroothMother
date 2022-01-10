1ef351c544ba6d37b835fc959ea4edc752579967
Kirk Daniel Caponpon <caponponkirk24@gmail.com>
2022-01-09 20:32:24 -0500

    Long Range Tower and PP Tower UI upgrades

---
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/Kirk C. Folder/UpgradeLR.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeLR : MonoBehaviour
+{
+    private void Start()
+    {
+        Hide();
+    }
+
+
+    public void Show()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+
+
+
+
+
+    public void sellPPtower()
+    {
+        
+    }
+
+    public void upgradespeed()
+    {
+
+    }
+
+    public void upgradedamage()
+    {
+
+    }
+}
---
+++ Brooth Mother (Fall 2021)(DoD)/Assets/Scenes/Kirk C. Folder/UpgradeLRButton.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpgradeLRButton : MonoBehaviour
+{
+    private bool EisPressed;
+    public UpgradeLR fornite;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E) && EisPressed)
+        {
+            fornite.Show();
+
+            Debug.Log("Player IN");
+        }
+
+    }
+   
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            EisPressed = true;
+            // Debug.Log("Player IN");
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            EisPressed = false;
+            Debug.Log("Player Out");
+            fornite.Hide();
+        }
+    }
+
+}
